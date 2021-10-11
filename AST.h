1c75947580eeaa17edc0666d7c3604b1701b5ba8
viniciusroratto <vini.jornal@gmail.com>
2021-10-11 20:38:10 -0300

    Correção do arquivo scanner.l

--- AST.h
+++ AST.h
@@ -5,6 +5,10 @@ Disciplina: INF01147 - Compiladores - Prof. Lucas Schnorr
 
 
 #include <stdio.h>
+#include <stdbool.h>
+
+extern void *arvore;
+
 
 union Valor {
 	int i;
@@ -14,7 +18,7 @@ union Valor {
 	bool b;
 } Valor;
 
-struct LexVal {
+typedef struct LexVal {
 	int linha;
 	int tipo;
  	union Valor val;
@@ -22,26 +26,25 @@ struct LexVal {
 
 /* O grupo entende que a solucao para arvore longe do modelo ideal para resolver este problema, o grupo pensou em uma arvore binaria com nodos filhos e nodos irmaos, 
 que conseguiria representar uma arvore N, no entanto esbarramos em desafios de implementacao
-como ter que enviar sempre quantidades diferentes de valores.
-Isso poderia ser resolvido com estruturas de dados de alto nivel em C++ como listas e vetores, no entanto nao conseguimos resolver a tempo.
+como ter que enviar sempre quantidades diferentes de valores. Isso poderia ser resolvido com estruturas de dados de alto nivel em C++ como listas e vetores, no entanto nao conseguimos resolver a tempo.
 */
-struct Nodo {
+typedef struct Nodo {
 
 	struct LexVal *val;
  	
- 	struct Nodo *f1
- 	struct Nodo *f2
- 	struct Nodo *f3
- 	struct Nodo *f4
- 	struct Nodo *f5
+ 	struct Nodo *f1;
+ 	struct Nodo *f2;
+ 	struct Nodo *f3;
+ 	struct Nodo *f4;
+ 	struct Nodo *f5;
  	
 } Nodo; 
 
 Nodo * criaNodo (LexVal *val, Nodo * n1, Nodo * n2, Nodo * n3, Nodo * n4, Nodo * n5){
 
-	Nodo* novo = (Nodo*) calloc(1, sizeof(nodo));
-	struct LexVal *val = *val;
+	Nodo* novo = (Nodo*) calloc(1, sizeof(Nodo));
 	
+	novo->val = val;
 	novo->f1 = n1;
 	novo->f2 = n2;
 	novo->f3 = n3;
@@ -51,43 +54,43 @@ Nodo * criaNodo (LexVal *val, Nodo * n1, Nodo * n2, Nodo * n3, Nodo * n4, Nodo *
 	return novo;
 }
 
-void imprime (void *arvore){
-	if (arvore != NULL){
-		printf("%p [label=\"Linha\"] \n"); //imprime o endereço de memoria; 
-		printf("%p [label=\"Tipo\"] \n");
-		imprime (arvore->f1);
-		imprime (arvore->f2);
-		imprime (arvore->f3);
-		imprime (arvore->f4);
-		imprime (arvore->f5); }
+void imprime (Nodo *newthree){
+	if (newthree != NULL){
+		//printf("%p [label=\"Linha\"] \n"); //imprime o endereço de memoria; 
+		//printf("%p [label=\"Tipo\"] \n");
+		imprime (newthree->f1);
+		imprime (newthree->f2);
+		imprime (newthree->f3);
+		imprime (newthree->f4);
+		imprime (newthree->f5); }
 }
 
-void exporta (void *arvore){
+void exporta (Nodo *newthree){
 	FILE *file = fopen("saida", "w");
-	if (arvore != NULL){
-		fprintf (file,"%p [label=\"Linha\"] \n"); //imprime o endereço de memoria;
-		fprintf (file,"%p [label=\"Tipo\"] \n");
-		exporta (arvore->f1);
-		exporta (arvore->f2); 
-		exporta (arvore->f3);
-		exporta (arvore->f4);
-		exporta (arvore->f5);
+	if (newthree != NULL){
+		//fprintf (file,"%p [label=\"Linha\"] \n"); //imprime o endereço de memoria;
+		//fprintf (file,"%p [label=\"Tipo\"] \n");
+		exporta (newthree->f1);
+		exporta (newthree->f2); 
+		exporta (newthree->f3);
+		exporta (newthree->f4);
+		exporta (newthree->f5);
 		
 		}
 	fclose (file);
 }
 
 
-void libera (void *arvore)
+void libera (Nodo * newthree)
 {
 	
 	if (arvore != NULL)
-	{	free(arvore)
-		libera(arvore->f1);
-		libera(arvore->f2);
-		libera(arvore->f3);
-		libera(arvore->f4);
-		libera(arvore->f5);	
+	{	free(newthree);
+		libera(newthree->f1);
+		libera(newthree->f2);
+		libera(newthree->f3);
+		libera(newthree->f4);
+		libera(newthree->f5);	
 	} 
 
 }
